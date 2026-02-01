b4f09370056ab07362702e46baa8b50ac7253aeb
Gangadhar C <cgangadhar@presidio.com>
2026-02-01 13:51:35 +0530

    refactor: update Vercel configuration to specify TypeScript entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,9 @@
 import { env } from './configs/env.config';
 import app from './app';
 
-app.listen({ port: env.PORT })
-app.log.info(`Server is running on port ${env.PORT}`);
+app.listen({ port: env.PORT }).then(() => {
+  app.log.info(`Server is running on port ${env.PORT}`);
+}).catch((err) => {
+   app.log.error(err);
+    process.exit(1);
+});
